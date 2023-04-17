e506e8f2d03e2577e6fc2f5092afca1444074e68
elmooe <elmo.erla@live.fi>
2023-04-17 22:03:07 +0300

    tehtävät 3.6 asti

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const nodemon = require('nodemon')
 const app = express()
+app.use(express.json())
+
+
+const generateId = () => {
+    const maxId = persons.length > 0 ? Math.max(...persons.map(person => person.id)) : 0
+    return maxId + 1
+}
+
+const generateNumber = () => {
+    return Math.floor(Math.random() * (39999999 - 39000000 + 1)) + 39000000;
+}
 
 let persons = [
   {
@@ -44,6 +55,31 @@ app.get('/api/persons/:id', (request, responce) => {
         responce.status(404).end()
     }
 })
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+  })
+
+  app.post('/api/persons', (request, responce) => {
+    const body = request.body
+
+    if (!body.name || persons.some(person => person.name === body.name)) {
+        return responce.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: generateNumber()
+    }
+
+    persons = persons.concat(person)
+    responce.json(person)
+  })
 
 const PORT = 3001
 app.listen(PORT, () => {
